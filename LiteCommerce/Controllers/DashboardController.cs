27469999811d7ebed71a69dfb516ab1d65c03886
Nguyen Van Hieu <hieunv@aureole.local>
2021-03-04 15:13:54 +0700

    1

---
+++ LiteCommerce/Controllers/DashboardController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LiteCommerce.Controllers
+{
+    //[Authorize] //kiểm tra đăng nhập
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
